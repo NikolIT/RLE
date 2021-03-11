f355328580b868097e63de07f54983b0dd0a4295
nikol <andrey.nikolaienko.0@gmail.com>
2021-03-11 13:29:27 +0200

    first commit

---
+++ RLE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<byte> test = new List<byte>()
+            {
+                12, 1, 1, 1, 5, 4, 2, 64, 1, 12, 8, 8, 8, 8
+            };
+
+
+            foreach (var item in RLE.Compression(test))
+            {
+                Console.Write($" {item}");  
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ RLE/RLE.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLE
+{
+    static class RLE
+    {
+        public  static List<byte> Compression(List<byte> byteImgList)
+        {
+
+            List<byte> resultList = new List<byte>();
+
+            resultList.Add(Byte.MinValue);
+
+            //List<byte> tempList = new List<byte>();
+            byte numberBytes1 = 1;//змінити назви
+            byte numberBytes2 = 1;
+            //byte tempbyte = byteImgList[0];
+            
+            for (int i = 1; i < byteImgList.Count; i++)
+            {
+                if (byteImgList[i - 1] == byteImgList[i])
+                {
+                    numberBytes2 = 1;
+                    if (numberBytes1 == 1)
+                        resultList.AddRange(new List<byte> { Byte.MinValue, Byte.MinValue });
+
+                    numberBytes1++;
+
+                    resultList[resultList.Count - 2] = numberBytes1;
+                    resultList[resultList.Count - 1] = byteImgList[i - 1];
+
+                    
+                    continue;
+                }
+                else
+                {
+                    numberBytes1 = 1;
+
+                    if(numberBytes2 == 1)
+                        resultList.AddRange(new List<byte> { Byte.MinValue, Byte.MinValue});
+
+                    resultList.Add(byteImgList[i - 1]);
+                    resultList[resultList.Count - (numberBytes2 + 1)] = numberBytes2;
+                    
+
+                    numberBytes2++;
+
+                    continue;
+                }
+
+
+            }
+
+
+            return resultList;
+        }
+    }
+}
