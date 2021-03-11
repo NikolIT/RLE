eb27de468fd04408f6c0cebd9d5da9c2dee665ac
nikol <andrey.nikolaienko.0@gmail.com>
2021-03-11 20:03:11 +0200

    'add_RLE_Unbuffered'

--- RLE/Program.cs
+++ RLE/Program.cs
@@ -16,8 +16,11 @@ namespace RLE
                 12, 1, 1, 1, 5, 4, 2, 64, 1, 12, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 132, 12, 43, 64, 99, 1, 12, 2, 2, 2, 2, 2, 2
             };
 
-            List<byte> boxingByte = RLE.Boxing(test);
-            List<byte> unboxingByte = RLE.Unboxing(boxingByte);
+            List<byte> boxingByteRLE = RLE.Boxing(test);
+            List<byte> unboxingByteRLE = RLE.Unboxing(boxingByteRLE);
+
+            List<byte> boxingByteRLE_U = RLE_Unbuffered.Boxing(test);
+            List<byte> unboxingByteRLE_U = RLE_Unbuffered.Unboxing(boxingByteRLE_U);
 
             Console.WriteLine("Origin:");
             foreach (var item in test)
@@ -25,17 +28,34 @@ namespace RLE
                 Console.Write($" {item}");
             }
 
+            Console.WriteLine("\n                   RLE");
+
             Console.WriteLine("\nBoxing:");
-            foreach (var item in boxingByte)
+            foreach (var item in boxingByteRLE)
             {
                 Console.Write($" {item}");  
             }
 
             Console.WriteLine("\nUnboxing:");
-            foreach (var item in unboxingByte)
+            foreach (var item in unboxingByteRLE)
             {
                 Console.Write($" {item}");
             }
+
+            Console.WriteLine("\n                   RLE unbuffered");
+
+            Console.WriteLine("\nBoxing:");
+            foreach (var item in boxingByteRLE_U)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine("\nUnboxing:");
+            foreach (var item in unboxingByteRLE_U)
+            {
+                Console.Write($" {item}");
+            }
+
             Console.Read();
         }
     }
---
+++ RLE/RLE_Unbuffered.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLE
+{
+    static class RLE_Unbuffered
+    {
+
+        public static List<byte> Boxing(List<byte> byteListInfOrig)
+        {
+            List<byte> result = new List<byte>();
+            List<byte> byteListInf = byteListInfOrig.ToList();
+
+            byteListInf.InsertRange(0, new List<byte> { Byte.MinValue, Byte.MinValue });
+            byte numberBytes = 0;
+
+            for (int i = 2; i < byteListInf.Count; i++)
+            {
+                if (byteListInf[i] == byteListInf[i - 1] && byteListInf[i] == byteListInf[i - 2])
+                {
+                    if (numberBytes == 0)
+                    {
+                        //result.AddRange(byteListInf.GetRange(i - 2, 2)); 
+                        result.Add(Byte.MinValue);
+                    }
+                        
+                    numberBytes++;
+                    result[result.Count - 1] = numberBytes;
+                }
+                else
+                {
+                    numberBytes = 0;
+                    result.Add(byteListInf[i]);
+                }
+            }
+
+            return result;
+        }
+
+        public static List<byte> Unboxing(List<byte> byteListInfOrig)
+        {
+            List<byte> result = new List<byte>();
+            List<byte> byteListInf = byteListInfOrig.ToList();
+
+            bool copy = false;
+
+            byteListInf.InsertRange(0, new List<byte> { Byte.MinValue, Byte.MaxValue, Byte.MinValue });
+
+            for (int i = 3; i < byteListInf.Count; i++)
+            {
+                if (byteListInf[i - 2] == byteListInf[i - 1] && !copy)
+                {
+                    int tempNum = byteListInf[i];
+                    byte tempByte = byteListInf[i - 1];
+                    //i += tempNum - 1;
+                    for (int j = 0; j < tempNum; j++)
+                    {
+                        result.Add(tempByte);
+                    }
+
+                    copy = true;
+                }
+                else
+                {
+                    copy = false;
+                    result.Add(byteListInf[i]);
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
