85cd63d80c47e9551230f90b47aca93c6cefa028
nikol <andrey.nikolaienko.0@gmail.com>
2021-03-11 15:10:15 +0200

    'rename_Compression_to_Boxing_and_add_Unboxing_method'

--- RLE/Program.cs
+++ RLE/Program.cs
@@ -16,16 +16,25 @@ namespace RLE
                 12, 1, 1, 1, 5, 4, 2, 64, 1, 12, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 132, 12, 43, 64, 99, 1, 12, 2, 2, 2, 2, 2, 2
             };
 
+            List<byte> boxingByte = RLE.Boxing(test);
+            List<byte> unboxingByte = RLE.Unboxing(boxingByte);
+
             Console.WriteLine("Origin:");
             foreach (var item in test)
             {
                 Console.Write($" {item}");
             }
 
-            Console.WriteLine("\n Compression:");
-            foreach (var item in RLE.Compression(test))
+            Console.WriteLine("\nBoxing:");
+            foreach (var item in boxingByte)
             {
                 Console.Write($" {item}");  
+            }
+
+            Console.WriteLine("\nUnboxing:");
+            foreach (var item in unboxingByte)
+            {
+                Console.Write($" {item}");
             }
             Console.Read();
         }
--- RLE/RLE.cs
+++ RLE/RLE.cs
@@ -8,22 +8,24 @@ namespace RLE
 {
     static class RLE
     {
-        public  static List<byte> Compression(List<byte> byteImgList)
+        public  static List<byte> Boxing(List<byte> byteListOrg)
         {
+
+            List<byte> byteList = byteListOrg.ToList();
 
             List<byte> resultList = new List<byte>();
 
             resultList.Add(Byte.MinValue);
-            byteImgList.Add(Byte.MinValue);
-            byteImgList.Insert(0, Byte.MinValue);
+            byteList.Add(Byte.MinValue);
+            byteList.Insert(0, Byte.MinValue);
             //List<byte> tempList = new List<byte>();
             byte numberBytes1 = 1;//змінити назви
             byte numberBytes2 = 1;
             //byte tempbyte = byteImgList[0];
             
-            for (int i = 1; i < byteImgList.Count - 1; i++)
+            for (int i = 1; i < byteList.Count - 1; i++)
             {
-                if (byteImgList[i - 1] == byteImgList[i] || byteImgList[i] == byteImgList[i + 1])
+                if (byteList[i - 1] == byteList[i] || byteList[i] == byteList[i + 1])
                 {
                     numberBytes2 = 1;
                     if (numberBytes1 == 1)
@@ -32,7 +34,7 @@ namespace RLE
                     
 
                     resultList[resultList.Count - 2] = numberBytes1;
-                    resultList[resultList.Count - 1] = byteImgList[i];
+                    resultList[resultList.Count - 1] = byteList[i];
 
                     numberBytes1++;
                     continue;
@@ -44,7 +46,7 @@ namespace RLE
                     if(numberBytes2 == 1)
                         resultList.AddRange(new List<byte> { Byte.MinValue, Byte.MinValue});
 
-                    resultList.Add(byteImgList[i]);
+                    resultList.Add(byteList[i]);
                     resultList[resultList.Count - (numberBytes2 + 1)] = numberBytes2;
                     
 
@@ -57,6 +59,39 @@ namespace RLE
             }
 
             resultList.RemoveAt(0);
+            return resultList;
+        }
+
+        public static List<byte> Unboxing(List<byte> byteList)
+        {
+            List<byte> resultList = new List<byte>();
+
+            int numbereBytes = 0;
+
+            for (int i = 0; i < byteList.Count; i++)
+            {
+                if (byteList[i] == 0)
+                {
+                    numbereBytes = byteList[i + 1];
+                    i++;
+                    for (int j = 0; j < numbereBytes; j++)
+                    {
+                        resultList.Add(byteList[++i]);
+                    }
+                }
+                else
+                {
+                    numbereBytes = byteList[i];
+                    byte temp = byteList[i + 1];
+                    i++;
+                    for (int j = 0; j < numbereBytes; j++)
+                    {  
+                        resultList.Add(temp);
+                    }
+                }
+
+            }
+
             return resultList;
         }
     }
